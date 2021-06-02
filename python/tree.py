d6b1bf91f300a4904c9fb66a4ce546aeb5300749
Jugal Mistry <jugalmistry99@gmail.com>
2021-06-02 21:39:07 +0530

    Create Different Nodes

--- python/tree.py
+++ python/tree.py
@@ -1,8 +1,34 @@
-from token import Token
-
 class Node:
-    def __init__(self, token: Token):
-        self.token = token
-        self.left = None
-        self.right = None
+    def __init__(self):
+        self.left: Node = None
+        self.right: Node = None
     
+class NumberNode(Node):
+    def __init__(self, value: float):
+        self.value = value
+
+    def __repr__(self):
+        return str(self.value)
+
+class OperatorNode(Node):
+    def __init__(self, operator: str):
+        self.operator = operator
+
+    def __repr__(self):
+        return f"({self.left} {self.operator} {self.right})"
+
+class AddNode(OperatorNode):
+    def __init__(self):
+        super().__init__('+')
+
+class SubtractNode(OperatorNode):
+    def __init__(self):
+        super().__init__('-')
+
+class MultiplyNode(OperatorNode):
+    def __init__(self):
+        super().__init__('*')
+
+class DivideNode(OperatorNode):
+    def __init__(self):
+        super().__init__('/')
